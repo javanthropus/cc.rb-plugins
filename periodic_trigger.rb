9b310f0d7fbc43e9601dfb622617d69712d26fc4
Jeremy Bopp <jeremy@bopp.net>
2011-10-10 13:34:21 -0500

    Initial commit

---
+++ periodic_trigger.rb
@@ -0,0 +1,31 @@
+# This is a trigger that will fire whenever the time since the last build of a
+# project exceeds a given period.
+#
+# Add this trigger using your @cruise_config.rb@ file:
+#
+# <pre><code>Project.configure do |project|
+#   ...
+#   project.triggered_by PeriodicTrigger.new(project, 5.minutes)
+#   ...
+# end</code></pre>
+#
+# When this trigger fires, it will notify the project with
+# @:wait_period_expired@.  Otherwise, it will notify the project with
+# @:wait_period_not_expired@.
+class PeriodicTrigger
+  def initialize(triggered_project, period)
+    @triggered_project = triggered_project
+    @period = period
+  end
+
+  def build_necessary?(reasons)
+    last_build = @triggered_project.last_build
+    if last_build && Time.now - last_build.time > @period
+      @triggered_project.notify :wait_period_expired
+      true
+    else
+      @triggered_project.notify :wait_period_not_expired
+      false
+    end
+  end
+end
