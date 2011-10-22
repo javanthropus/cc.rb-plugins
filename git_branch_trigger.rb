4632a0e5aec5e74c2b6cfe601e0aa6e492624427
Jeremy Bopp <jeremy@bopp.net>
2011-10-22 13:38:07 -0500

    Store branch lists in memory rather than a file

--- git_branch_trigger.rb
+++ git_branch_trigger.rb
@@ -17,26 +17,17 @@
 #   ...
 # end</code></pre>
 #
-# This trigger will not fire the first time it is evaluated for a project.  It
-# will only record the current list of branches for future reference.
-# Thereafter, it will update the saved list of branches each time it is
-# evaluated.
+# This trigger will not fire the first time it is evaluated after the builder
+# starts.  It will only record the current list of branches for future
+# reference.  Thereafter, it will fire when changes are seen.
 class GitBranchTrigger
   def initialize(project, git_url)
     @project = project
     @git_url = git_url
+    @branch_list = nil
   end
 
   def build_necessary?(reasons)
-    branch_list_file = File.join(@project.path, "branch.list")
-
-    # Retrieve the old list of branches if there is one.
-    begin
-      old_branch_list = File.readlines(branch_list_file).map(&:chomp)
-    rescue Errno::ENOENT
-      # Ignored... First run for this project.
-    end
-
     # Retrieve the current list of branches.
     new_branch_list = IO.popen("git ls-remote --heads #{@git_url} 2>&1") do |git|
       git.readlines
@@ -52,8 +43,9 @@ class GitBranchTrigger
     new_branch_list.map! { |l| l.chomp.split(/\s+/)[1] }
     new_branch_list.sort!
 
-    # Save the list.
-    File.open(branch_list_file, "w") { |f| f.puts new_branch_list }
+    # Update the saved branch list with the current branches.
+    old_branch_list = @branch_list
+    @branch_list = new_branch_list
 
     # Trigger the build only if there is a change in the branch list.
     if old_branch_list.nil? || old_branch_list == new_branch_list
